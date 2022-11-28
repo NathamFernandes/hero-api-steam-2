80be364f4c8e415a273815d16246480a5a06ac27
Natham Fernandes <natham.fr@gmail.com>
2022-11-27 22:35:04 -0300

    teste1

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const SteamAPI = require('steamapi');
-const steam = new SteamAPI('0D372211EAD5CDB42F5A686BF51766CC');
+const bodyParser = require('body-parser');
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -7,44 +6,35 @@ const port = 3000;
 
 app.use(cors());
 
+const fetch = (...args) =>
+    import('node-fetch').then(({ default: fetch }) => fetch(...args));
+
 app.get('/', (req, res) => {
     res.send("Tudo normal!")
 })
 
-app.get('/teste1', (req, res) => {
-    steam.resolve('https://steamcommunity.com/profiles/76561198148730868').then(id => { console.log(id); })
-})
+app.get('/usuarios/:idusuario', async (req, res) => {
+    let url = `http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730&key=0D372211EAD5CDB42F5A686BF51766CC&steamid=${req.params.idusuario}`
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Host': 'api.steampowered.com',
+            'X-RapidAPI-Key': '0D372211EAD5CDB42F5A686BF51766CC'
+        }
+    };
 
-app.get('/teste2', (req, res) => {
-    steam.resolve('https://steamcommunity.com/profiles/76561198248619940').then(id => { console.log(id); })
-})
-
-app.get('/teste3', (req, res) => {
-    steam.getUserSummary('76561198148730868').then(summary => { console.log(summary); })
-})
-
-app.get('/teste4', (req, res) => {
-    steam.getUserSummary('76561198248619940').then(summary => { console.log(summary); })
-})
-
-app.get('/teste5', (req, res) => {
-    steam.getUserStats('76561198148730868', 730).then(stats => { console.log(stats); })
-})
-
-app.get('/teste6', (req, res) => {
-    steam.getUserStats('76561198248619940', 730).then(stats => { console.log(stats); })
-})
-
-app.get('/teste7', (req, res) => {
-    steam.getUserStats('76561198148730868', 730).then(stats => { res.json(stats); })
-})
-
-app.get('/teste8', (req, res) => {
-    steam.getUserStats('76561198248619940', 730).then(stats => { res.json(stats); })
-})
-
-app.get('/usuarios/:idusuario', (req, res) => {
-    steam.getUserStats(req.params.idusuario, 730).then(stats => { res.json(stats); })
+    fetch(url, options)
+        .then(res => res.json())
+        .then(json => console.log(json))
+        .catch(err => console.error('error:' + err));
+    try {
+        let response = await fetch(url, options);
+        response = await response.json();
+        res.status(200).json(response);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ msg: `Internal Server Error.` });
+    }
 });
 
 app.listen(port, () => {
